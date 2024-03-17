50da548f91cc684ffdc4e4eca4b4da4cf364c00e
Ivan Lebedev <ivan.lebedev.graphics@gmail.com>
2024-03-18 02:08:10 +0300

    Some edits.

--- setup_workstation.py
+++ setup_workstation.py
@@ -1,280 +1,235 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, shutil, time
+import os, sys, subprocess
+
+
+# DISKs INFO
+def get_volumes():
+
+    # Get the list of DEVICES
+    lsblk = subprocess.check_output('lsblk').decode('UTF-8').splitlines()
+
+#    lsblk = 'sdg1        8:97   0  12,5T  0 part \nsdh           8:112  0  12,7T  0 disk \n└─sdh1        8:113  0  12,5T  0 part \nsdi           8:128  0  12,7T  0 disk \n└─sdi1        8:129  0  12,5T  0 part \nsdj           8:144  0  12,7T  0 disk \n└─sdj1        8:145  0  12,5T  0 part \nsdk           8:160  0  12,7T  0 disk \n└─sdk1        8:161  0  12,5T  0 part \nsdl           8:176  0  12,7T  0 disk \n└─sdl1        8:177  0  12,5T  0 part \nsdm           8:192  1     0B  0 disk \nsr0          11:0    1  1024M  0 rom  \nnvme1n1     259:0    0 232,9G  0 disk \n├─nvme1n1p1 259:1    0    75G  0 part \n└─nvme1n1p2 259:2    0 157,9G  0 part \nnvme0n1     259:3    0 476,9G  0 disk \n├─nvme0n1p1 259:4    0     1G  0 part \n├─nvme0n1p2 259:5    0   256G  0 part \n├─nvme0n1p3 259:6    0    40G  0 part \n├─nvme0n1p4 259:7    0    40G  0 part \n├─nvme0n1p5 259:8    0    40G  0 part \n└─nvme0n1p6 259:9    0  99,9G  0 part \n'
+ #   lsblk = lsblk.splitlines()
+
+    devs = []
+    for line in lsblk:
+        if not line.find('├') or not line.find('└'):
+            devs.append(line[2:])
+
+    vols = []
+    for line in devs:
+        parts = line.split()
+        if len(parts) >= 6:
+            volume = {
+                'name': parts[0],
+                'size': parts[3],
+                'mountpoint': parts[6] if len(parts) > 6 else None
+            }
+            vols.append(volume)
+
+    # Print list of found VOLUMEs
+    print('=' * 40)
+    print('Found VOLUMEs:')
+    print('=' * 40)
+
+    for volume in vols:
+        if len(volume['name']) <= 8:
+            print(volume['name'], '\t\t', volume['size'], '\t\t', volume['mountpoint'])
+        else:
+            print(volume['name'], '\t', volume['size'], '\t\t', volume['mountpoint'])
+
+    return vols
+
 
 # FORMAT VOLUMES
-def format_volumes():
+def format_volumes(vols):
+
+    # Check for mounted VOLUMEs
+    for volume in vols:
+        if volume['mountpoint'] != None:
+            if input('\nWARNING! Some volumes are mounted. Continue? [y/N] ').lower() == 'y':
+                break
+            else:
+                sys.exit()
+
+    # Format VOLUMEs
+    print('=' * 40)
+    print('Format VOLUMEs:')
+    print('=' * 40)
+
+    for volume in vols:
+        if volume['mountpoint'] == None:
+            match volume['name']:
+                case 'nvme0n1p1':
+                    if input('Format /dev/nvme0n1p1? [y/N] ').lower() == 'y':
+#                        os.system('mkfs.vfat /dev/nvme0n1p1')
+                        print('Formatting /dev/nvme0n1p1 as FAT32')
+                case 'nvme0n1p2':
+                    if input('Format /dev/nvme0n1p2? [y/N] ').lower() == 'y':
+#                        os.system('mkswap /dev/nvme0n1p2')
+                        print('Formatting /dev/nvme0n1p2 as SWAP')
+                case _:
+                    if input('Format /dev/' + volume['name'] + '? [y/N] ').lower() == 'y':
+#                        os.system('mkfs.ext4 /dev/' + volume['name'])
+                        print('Formatting /dev/' + volume['name'] + ' as EXT4')
+        else:
+            print('The ' + volume['name'] +' is mounted. Skipping!')
+
+
+# MOUNT VOLUMEs
+def mount_volumes(hostname):
+
+    # Mount VOLUMEs
+    print('=' * 40)
+    print('Mount VOLUMEs:')
+    print('=' * 40)
+
+    print('Mounting /dev/nvme0n1p2 at SWAP')
+#    os.system('swapon /dev/nvme0n1p2')
+    print('Mounting /dev/nvme0n1p3 at /mnt')
+#    os.system('mount /dev/nvme0n1p3 /mnt')
+    print('Creating /mnt/boot')
+#    os.mkdir('/mnt/boot')
+    print('Mounting /dev/nvme0n1p1 at /mnt/boot')
+#    os.system('mount /dev/nvme0n1p1 /mnt/boot')
+    print('Creating /mnt/var')
+#    os.mkdir('/mnt/var')
+    print('Mounting /dev/nvme0n1p4 at /mnt/boot')
+#    os.system('mount /dev/nvme0n1p4 /mnt/var')
+    print('Creating /mnt/tmp')
+#    os.mkdir('/mnt/tmp', 0o777)
+    print('Mounting /dev/nvme0n1p5 at /mnt/tmp')
+#    os.system('mount /dev/nvme0n1p5 /mnt/tmp')
+    print('Creating /mnt/home')
+#    os.mkdir('/mnt/home')
+    print('Mounting /dev/nvme0n1p6 at /mnt/home')
+#    os.system('mount /dev/nvme0n1p6 /mnt/home')
+    print('Creating /mnt/mnt/' + hostname)
+#    os.makedirs('/mnt/mnt/' + hostname)
+    print('Mounting /dev/nvme0n1p7 at /mnt/mnt/' + hostname)
+#    os.system('mount /dev/nvme0n1p7 /mnt/mnt/' + hostname)
+
+
+# PACSTRAP
+def pacstrap(hostname):
+
+    print('=' * 40)
+    print('PACSTRAP:')
+    print('=' * 40)
+
+    print('Install base packages')
+#    os.system('pacstrap /mnt base base-devel linux-lts linux-lts-headers linux-firmware python neovim nfs-utils dkms bash-completion man openssh rsync reflector terminus-font')
+
+    if input('\nChoose ucode type: 1 - AMD, 2 - Intel? [1 - default] ').lower() == '2':
+        print('Installing intel-ucode')
+#        os.system('pacstrap /mnt intel-ucode')
+        ucode = 'intel-ucode.img'
+    else:
+        print('Install amd-ucode')
+#        os.system('pacstrap /mnt amd-ucode')
+        ucode = 'amd-ucode.img'
+
+    print('\nGenerate /etc/fstab')
+#    os.system('genfstab -U /mnt >> /mnt/etc/fstab')
+
+    print('Arch-Chroot /mnt')
+#    os.system('arch-chroot /mnt')
+
+    print('Link /etc/localtime')
+#    os.system('ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime')
+
+    print('Sync hardware clock')
+#    os.system('hwclock --systohc')
+
+    print('Mount vfxserver01/Tools at /mnt')
+#    os.system('mount -t nfs 192.168.20.11:/mnt/vfxserver01/Tools /mnt')
 
-    volumes = subprocess.check_output('lsblk')
+    print('Setup locale to ru_RU.UTF-8')
+#    os.system('cp -v /mnt/_configuration_files/etc/locale* /etc')
+#    os.system('cp -v /mnt/_configuration_files/etc/vconsole.conf /etc')
+#    locale-gen
 
+    print('Setup hostname')
+#    with open('/etc/hostname', 'w') as f:
+#        f.write(hostname)
 
+    print('Setup /etc/modprobe.d/blacklist.conf')
+#    os.system('cp -v /mnt/_configuration_files/etc/modprobe.d/blacklist.conf /etc')
 
-# MOUNT SERVERS
-def setup_automount ():
+    print('Setup /etc/systemd/system/systemd-networkd.service')
+#    os.system('cp -v /mnt/_configuration_files/etc/systemd/network/20-wired.network /etc/systemd/network')
+#    os.system('systemctl enable systemd-networkd.service')
 
-    if not os.path.exists('/mnt/vfxserver01'):
-        os.mkdir('/mnt/vfxserver01', 0o755)
-    
-    if not os.path.exists('/mnt/vfxcache01'):
-        os.mkdir('/mnt/vfxcache01', 0o755)
+    print('Setup /etc/systemd/system/systemd-resolved.service')
+#    os.system('systemctl enable systemd-resolved.service')
 
-    if not os.path.exists('/mnt/vfxstorage01'):
-        os.mkdir('/mnt/vfxstorage01', 0o755)
+    print('Setup /etc/systemd/system/sshd.service')
+#    os.system('systemctl enable sshd.service')
 
-    if not os.path.exists('/mnt/vfxstorage02'):
-        os.mkdir('/mnt/vfxstorage02', 0o755)
+    print('Setup /etc/systemd/system/systemd-timesyncd.service')
+#    os.system('cp -v /mnt/_configuration_files/etc/systemd/timesyncd.conf /etc/systemd')
 
-    os.system('mount -t nfs 192.168.20.11:/mnt/vfxserver01 /mnt/vfxserver01')
-    os.system('cp -v /mnt/vfxserver01/Tools/_configuration_files/etc/systemd/system/mnt-* /etc/systemd/system/')
-    time.sleep(1)
+    print('Setup /etc/pacman.d/mirrorlist via Reflector')
+#    os.system('reflector -c Russia --sort rate --save /etc/pacman.d/mirrorlist')
 
-    os.system('umount /mnt/vfxserver01')
-    time.sleep(1)
+    print('Set root password')
+#    os.system('passwd')
 
-    os.system('systemctl daemon-reload')
-    os.system('systemctl enable --now mnt-vfxcache01.mount')
-    os.system('systemctl enable --now mnt-vfxserver01.mount')
-    os.system('systemctl enable --now mnt-vfxstorage01.mount')
-    os.system('systemctl enable --now mnt-vfxstorage02.mount')
-    time.sleep(1)
+    print('Setup bootloader')
+#    os.system('bootctl install')
+#    os.system('cp -v /mnt/_configuration_files/boot/loader/loader.conf /boot/loader')
 
-    print('Setting up automount ... Done')
+    blkid = subprocess.check_output('blkid').decode('UTF-8').splitlines()
+    uuid = ''
+    for line in blkid:
+        if line.find('nvme0n1p3'):
+            uuid = line.split()[1]
+            break
 
-# MOUNT PACMAN CACHE
-def setup_pacman_cache ():
+#    with open('/boot/loader/entries/arch.conf', 'w') as f:
+#        f.write('title   Arch Linux\n')
+#        f.write('linux   /vmlinuz-linux-lts\n')
+#        f.write('initrd  /initramfs-linux-lts.img\n')
+#        f.write('initrd  /' + ucode + '\n')
+#        f.write('options root=' + uuid + ' rw nowatchdog loglevel=3 nvidia_drm.modeset=1')
 
-    path = '/var/cache/pacman/pkg'
+    print('Copy setup_vfxstation.py to /root')
+#    os.system('cp -v /mnt/_shell_tools/setup_vfxstation.py /root')
 
-    if os.path.exists(path):
-        if input('Pacman cache directory is not empty. Do you want to clear it? [y/N]: ').lower() == 'y':
-            try:
-                shutil.rmtree(path)
-                time.sleep(1)
-                os.system('cp -v /mnt/vfxserver01/Tools/_configuration_files/etc/systemd/system/var-* /etc/systemd/system/')
-                os.system('systemctl enable --now var-cache-pacman.mount')
-            except OSError as e:
-                print("Error: %s : %s" % (path, e.strerror))
+    print('Unmount /mnt/vfxserver01')
+#    os.system('umount /mnt')
 
-    time.sleep(1)
-    print('Setting up pacman cache ... Done')
+    print('Exit')
+#    os.system('exit')
 
-# SETUP HOSTS
-def setup_hosts ():
+    print('Unmount /mnt')
+#    os.system('umount -R /mnt')
 
-    hostname = os.uname().nodename
+    print('\nFinished!\nSystem ready to reboot.')
 
-    os.system('cp -v /mnt/vfxserver01/Tools/_configuration_files/etc/hosts /etc/')
-
-    with open('/etc/hosts', 'r') as file :
-        filedata = file.read()
-
-    filedata = filedata.replace('vfxhost', hostname)
-
-    with open('/etc/hosts', 'w') as file :
-        file.write(filedata)
-
-    time.sleep(1)
-    print('Setting up /etc/hosts ... Done')
-
-# SOFT
-def install_soft ():
-
-    # COMMON SOFT
-    os.system('pacman -S --needed --noconfirm sudo man tmux rsync bash-completion exfat-utils ntfs-3g unrar unzip p7zip dkms duf htop util-linux ethtool numactl')
-
-    # SOFT FOR AFRENDER
-    if input('Install soft for afrender? [y/N] ').lower() == 'y':
-        os.system('pacman -S --needed --noconfirm glu alsa-lib nss freetype2 fontconfig libglvnd libxv libxmu libxi libxtst libxcomposite libxcursor libxrender libxkbcommon ttf-liberation')
-        time.sleep(1)
-
-        # AFRENDER SERVICE
-        if not os.path.exists('/usr/lib/systemd/system/afrender.service'):
-            if input('Setup afrender sevice? [y/N] ').lower() == 'y':
-
-                path = '/mnt/vfxserver01/Tools/_configuration_files/etc/systemd/system/afrender.service'
-
-                if os.path.exists(path):
-                    try:
-                        os.system('cp -v /mnt/vfxserver01/Tools/_configuration_files/etc/systemd/system/afrender.service /etc/systemd/system/')
-                        os.system('cp -v /mnt/vfxserver01/Tools/_configuration_files/etc/systemd/system/stop_hserver.service /etc/systemd/system/')
-                        os.system('systemctl enable afrender.service')
-                        os.system('systemctl enable stop_hserver.service')
-                        time.sleep(1)
-                    except OSError as e:
-                        print("Error: %s : %s" % (path, e.strerror))
-
-        print("Installing soft for afrender ... Done")
-
-    # SOFT FOR VFXSTATION
-    if input('Install soft for vfxstation? [y/N] ').lower() == 'y':
-        os.system('pacman -S --needed --noconfirm sddm plasma-desktop plasma-pa plasma-systemmonitor kdeplasma-addons breeze breeze-gtk kde-gtk-config konsole dolphin pipewire pipewire-pulse okular spectacle filezilla firefox firefox-ublock-origin ttf-dejavu ttf-liberation ttf-bitstream-vera cantarell-fonts ark kscreen krename kate ktorrent kolourpaint kdenlive mpv mediainfo inkscape python-pyqt5 python-lxml telegram-desktop nvidia-dkms opencl-nvidia pyside2 qt5-xmlpatterns hddtemp psensor')
-
-        f = open('/etc/sddm.conf', 'w')
-        f.write('[Theme]\nCurrent=breeze\n\n')
-        f.write('[Users]\nMaximumUid=99999\nMinimumUid=99999')
-        f.close()
-
-        os.system('systemctl enable sddm')
-        time.sleep(1)
-        print("Installing software for vfxstation ... Done")
-
-# LINKING SOFT
-def link_soft ():
-
-    os.symlink('/mnt/vfxserver01/Programs', '/soft')
-
-    os.remove('/etc/skel/.bashrc')
-    os.symlink('/mnt/vfxserver01/Tools/_user_preferences/bashrc', '/etc/skel/.bashrc')
-
-    # Nuke Preferences
-    os.mkdir('/etc/skel/.nuke')
-    os.symlink('/mnt/vfxserver01/Tools/Nuke/menu.py', '/etc/skel/.nuke/menu.py')
-    os.symlink('/mnt/vfxserver01/Tools/Nuke/init.py', '/etc/skel/.nuke/init.py')
-
-    # Houdini Preferences
-    os.symlink('/mnt/vfxserver01/Tools/_user_preferences/sesi_licenses.pref', '/etc/skel/.sesi_licenses.pref')
-
-    # Soft
-    os.mkdir('/etc/opt')
-    os.symlink('/soft/cgru/cgru', '/opt/cgru')
-    os.symlink('/soft/isl', '/opt/isl')
-    os.symlink('/soft/isl', '/etc/opt/isl')
-    os.symlink('/soft/OFX', '/usr/OFX')
-    os.symlink('/soft/houdini', '/opt/houdini')
-    os.symlink('/soft/foundry', '/usr/local/foundry')
-    os.symlink('/soft/nuke/Nuke15.0v2', '/usr/local/Nuke15.0v2')
-
-    # Additional links
-    os.symlink('/lib/libidn.so.12', '/lib/libidn.so.11')
-    os.symlink('/lib/libcrypt.so.2', '/lib/libcrypt.so.1')
-    os.symlink('/lib/libpython3.11.so', '/lib/libpython3.9.so')
-    os.symlink('/lib/libpython3.11.so.1.0', '/lib/libpython3.9.so.1.0')
-    os.symlink('/lib/libpython3.11.so', '/lib/libpython3.10.so')
-    os.symlink('/lib/libpython3.11.so.1.0', '/lib/libpython3.10.so.1.0')
-
-    time.sleep(1)
-    print("Linking software ... Done")
-
-# SUSPEND & HIBERNATE
-def stop_sleep_suspend_and_hibernate ():
-
-    os.system('systemctl mask suspend.target hibernate.target suspend-then-hibernate.target hybrid-sleep.target')
-
-    time.sleep(1)
-    print("Stopping sleep, suspend, hibernate modes ... Done")
-
-# TRIM & SENSORS
-def setup_trim_and_sensors ():
-
-    os.system('sensors-detect')
-    os.system('systemctl enable fstrim.timer') 
-
-    time.sleep(1)
-    print("Enabling TRIM and lm_sensors ... Done")
-
-# USERS
-def create_users ():
-
-    if input('Create master user? (Y/n) ').lower() in {'y', ''}:
-        os.system('useradd -G wheel,power,video,storage,lp,input,audio -s /bin/bash -m master')
-        os.system('passwd master')
-
-    if input('Create render user? (Y/n) ').lower() in {'y', ''}:
-        os.system('useradd -g 1000 -s /bin/bash -m render')
-
-    if input('Create user? (Y/n) ').lower() in {'y', ''}:
-        user = input('Enter user name: ').lower()
-        os.system('useradd -g 1000 -G  power,video,storage,lp,input,audio -s /bin/bash -m ' + user)
-        os.system('passwd ' +  user)
-
-    time.sleep(1)
-    print("Creating users ... Done")
-
-# BASE DIRECTORIES
-def create_base_directories ():
-
-    hostname = os.uname().nodename
-
-    os.mkdir('/mnt/' + hostname + '/Temp', 0o777)
-    os.mkdir('/mnt/' + hostname + '/Temp/Media', 0o777)
-    os.mkdir('/mnt/' + hostname + '/Temp/MoTemp', 0o777)
-    os.mkdir('/mnt/' + hostname + '/Temp/Blender', 0o777)
-    os.mkdir('/mnt/' + hostname + '/Temp/Houdini', 0o777)
-    os.mkdir('/mnt/' + hostname + '/Temp/Nuke', 0o777)
-    os.mkdir('/mnt/' + hostname + '/Temp/Nuke/nuke-u1000', 0o777)
-    os.mkdir('/mnt/' + hostname + '/Temp/Nuke/nuke-u1001', 0o777)
-    os.mkdir('/mnt/' + hostname + '/Temp/Nuke/nuke-u1002', 0o777)
-
-    os.system('chmod -R 777 /mnt/' + hostname + '/Temp')
-
-    time.sleep(1)
-    print("Creating base directories ... Done")
-
-# MENU ICONS
-def create_icons ():
-
-    icons_path = '/mnt/vfxserver01/Tools/_configuration_files/usr/share/applications/'
-
-    if os.path.exists(icons_path):
-
-        try:
-            icons = os.listdir(icons_path)
-
-            for icon in icons:
-
-                src = icons_path + icon
-                des = '/usr/share/applications/' + icon
-
-                if not os.path.exists(des):
-                    os.symlink(src, des)
-
-        except OSError as e:
-            print("Error: %s : %s" % (icons_path, e.strerror))
-
-    time.sleep(1)
-    print("Creating menu icons ... Done")
 
 # MAIN
 def main ():
 
     os.system('timedatectl set-ntp true')
 
-    if input('Start pacstrap? [y/N]: ').lower() == 'y':
-        os.system('pacstrap /mnt base base-devel linux-lts linux-lts-headers linux-firmware')
+    while True:
+        hostname = input('\nPlease enter the hostname for this station: ')
+        if input('The hostname is ' + hostname + '. Correct? [y/N] ').lower() == 'y':
+            break
+    
+    vols = get_volumes()
 
-        if input('Choose ucode type: 1 - Intel, 2 - AMD? ').lower() == '2':
-            os.system('pacstrap /mnt amd-ucode')
-        else:
-            os.system('pacstrap /mnt intel-ucode')
+    if input('\nFormat disks? [y/N] ').lower() == 'y':
+        format_volumes(vols)
 
-
-    if input('Setup pacman cache directory? [y/N] ').lower() == 'y':
-        setup_pacman_cache()
-
-    if input('Setup /etc/hosts? [y/N] ').lower() == 'y':
-        setup_hosts()
-
-    if input('Install software? [y/N] ').lower() == 'y':
-        install_soft()
-
-    if input('Create links to the programs? [y/N] ').lower() == 'y':
-        link_soft()
-
-    if input('Disable suspend & hibernate modes? [y/N] ').lower() == 'y':
-        stop_sleep_suspend_and_hibernate()
-
-    if input('Enable TRIM and lm_sensors? [y/N] ').lower() == 'y':
-        setup_trim_and_sensors()
-
-    if input('Create users? [y/N] ').lower() == 'y':
-        create_users()
-
-    if input('Create base directories? [y/N] ').lower() == 'y':
-        create_base_directories()
-
-    if input('Create menu icons for applications? [y/N] ').lower() == 'y':
-        create_icons()
+    if input('\nMount disks? [y/N] ').lower() == 'y':
+        mount_volumes(hostname)
+        
+    if input('\nStart pacstrap? [y/N]: ').lower() == 'y':
+        pacstrap(hostname)
 
 if __name__ == '__main__':
     main()
