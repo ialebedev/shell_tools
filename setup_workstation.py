aca3d40f39bd4aa177cf4f3223eed8a0bb90f890
Ivan Lebedev <ivan.lebedev.graphics@gmail.com>
2024-03-25 14:50:12 +0300

    Some decore.

--- setup_workstation.py
+++ setup_workstation.py
@@ -214,9 +214,6 @@ def pacstrap(hostname):
         f.write(data)
         f.truncate()
     os.system('arch-chroot /mnt reflector -c Russia --sort rate --latest 5 --save /etc/pacman.d/mirrorlist')
- 
-
-
 
     print('\nSetting root password')
     os.system('arch-chroot /mnt passwd')
