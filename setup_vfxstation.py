b50a397128c526635308a6cdbf7feef2ec95a60d
Ivan Lebedev <ivan.lebedev.graphics@gmail.com>
2024-08-04 00:56:24 +0300

    Fixes

--- setup_vfxstation.py
+++ setup_vfxstation.py
@@ -141,10 +141,10 @@ def link_soft ():
     # Additional links
     os.symlink('/lib/libidn.so.12', '/lib/libidn.so.11')
     os.symlink('/lib/libcrypt.so.2', '/lib/libcrypt.so.1')
-    os.symlink('/lib/libpython3.12.so', '/lib/libpython3.9.so')
     os.symlink('/lib/libpython3.12.so.1.0', '/lib/libpython3.9.so.1.0')
-    os.symlink('/lib/libpython3.12.so', '/lib/libpython3.10.so')
     os.symlink('/lib/libpython3.12.so.1.0', '/lib/libpython3.10.so.1.0')
+    os.symlink('/lib/libpython3.12.so.1.0', '/lib/libpython3.11.so.1.0')
+
 
     time.sleep(1)
     print("Linking software ... Done")
