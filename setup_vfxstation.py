edbbcdfc17f9da81e812c39bd3ca93c9832f30f8
Ivan Lebedev <ivan.lebedev.graphics@gmail.com>
2024-09-22 02:14:03 +0300

    Added obsidian and double commander to apps.

--- setup_vfxstation.py
+++ setup_vfxstation.py
@@ -101,7 +101,7 @@ def install_soft ():
 
     # SOFT FOR VFXSTATION
     if input('Install soft for vfxstation? [y/N] ').lower() == 'y':
-        os.system('pacman -S --needed --noconfirm sddm plasma-desktop plasma-pa plasma-systemmonitor kdeplasma-addons breeze breeze-gtk kde-gtk-config konsole dolphin pipewire pipewire-pulse okular spectacle filezilla firefox firefox-ublock-origin ttf-dejavu ttf-liberation ttf-bitstream-vera cantarell-fonts ark kscreen krename kate ktorrent kolourpaint kdenlive mpv mediainfo inkscape python-pyqt5 python-lxml telegram-desktop nvidia-dkms opencl-nvidia qt5-xmlpatterns hddtemp psensor')
+        os.system('pacman -S --needed --noconfirm sddm plasma-desktop plasma-pa plasma-systemmonitor kdeplasma-addons breeze breeze-gtk kde-gtk-config konsole dolphin pipewire pipewire-pulse okular spectacle filezilla firefox firefox-ublock-origin ttf-dejavu ttf-liberation ttf-bitstream-vera cantarell-fonts ark kscreen krename kate ktorrent kolourpaint kdenlive mpv mediainfo inkscape python-pyqt5 python-lxml telegram-desktop nvidia-dkms opencl-nvidia qt5-xmlpatterns hddtemp psensor obsidian doublecmd-qt6')
 
         f = open('/etc/sddm.conf', 'w')
         f.write('[Theme]\nCurrent=breeze\n\n')
@@ -142,8 +142,6 @@ def link_soft ():
     os.symlink('/lib/libidn.so.12', '/lib/libidn.so.11')
     os.symlink('/lib/libcrypt.so.2', '/lib/libcrypt.so.1')
     os.symlink('/lib/libpython3.12.so.1.0', '/lib/libpython3.9.so.1.0')
-    os.symlink('/lib/libpython3.12.so.1.0', '/lib/libpython3.10.so.1.0')
-    os.symlink('/lib/libpython3.12.so.1.0', '/lib/libpython3.11.so.1.0')
 
 
     time.sleep(1)
