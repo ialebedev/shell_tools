7feafe140bd3900e9398b661d7f46000e326a8e8
Ivan Lebedev <ivan.lebedev.graphics@gmail.com>
2025-07-22 12:12:26 +0300

    Some fixes.

--- zfsbackup.py
+++ zfsbackup.py
@@ -3,37 +3,60 @@
 # uv add zfslib
 
 import os
+import sys
 import subprocess
 
 from colorama import Fore, Style
 
 
-def get_pools() -> []:
+def message(message: str, status: bool):
+    if status:
+        print(f"{message:<70}[ {Fore.GREEN} OK {Style.RESET_ALL} ]")
+    else:
+        print(f"{message:<70}[ {Fore.RED}Fail{Style.RESET_ALL} ]")
+
+
+def check_pool(pool: str) -> bool:
     output = subprocess.run("zpool list", shell=True, capture_output=True, text=True)
     lines = output.stdout.splitlines()
 
-    pools = [line.split()[0] for line in lines[1:]]
-    print(f"Found ZFS pools: {', '.join(pools)}")
+    for line in lines[1:]:
+        if pool == line.split()[0]:
+            message(f"Found {pool} pool", True)
+            return True
 
-    return pools
+    message(f"No {pool} pool found. Exit", False)
 
-def get_datasets(pools: list[str]) -> list[str]:
-    for pool in pools:
-        output = subprocess.run("zfs list", shell=True, capture_output=True, text=True)
-        lines = output.stdout.splitlines()
+    return False
 
-        datasets = [line.split()[0] for line in lines[1:]]
-        print(f"Found ZFS datasets: {', '.join(datasets)}")
-    
+
+def get_datasets(pool: str) -> list[str]:
+    output = subprocess.run(
+        "zfs list -r " + pool, shell=True, capture_output=True, text=True
+    )
+    lines = output.stdout.splitlines()
+
+    datasets = [line.split()[0] for line in lines[2:]]
+    if datasets:
+        message(f"Found datasets in {pool}", True)
+
+        # for dataset in datasets:
+        #     print(dataset)
+    else:
+        message(f"No datasets found in {pool}. Exit", False)
+
+    return datasets
+
 
 def zfsbackup(host):
-    print(
-        f"Starting backup scenario for {host}\t\t[ {Fore.GREEN} OK {Style.RESET_ALL} ]"
-    )
+    message(f"Starting backup scenario for {host}", True)
 
-    pools = get_pools()
-    datasets = get_datasets(pools)
-    
+    if check_pool("zdata"):
+        datasets = get_datasets("zdata")
+    else:
+        sys.exit()        
+
+
 
 # MAIN
 def main():
