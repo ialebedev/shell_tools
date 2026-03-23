3b496e335d587160cda7f50cdbc8eecd0228ed9b
Ivan Lebedev <ivan.lebedev.graphics@gmail.com>
2026-03-23 17:51:56 +0300

    Some fixes

--- setup_vfxstation.py
+++ setup_vfxstation.py
@@ -109,7 +109,7 @@ def install_soft():
     # SOFT FOR VFXSTATION
     if input("Install soft for vfxstation? [y/N] ").lower() == "y":
         os.system(
-            "pacman -S --needed --noconfirm sddm plasma-desktop plasma-pa plasma-systemmonitor kdeplasma-addons breeze breeze-gtk kde-gtk-config konsole thunar thunar-volman gvfs thunar-archive-plugin pipewire pipewire-pulse okular spectacle filezilla firefox firefox-ublock-origin ttf-dejavu ttf-liberation ttf-bitstream-vera cantarell-fonts ark kscreen krename kate ktorrent kolourpaint kdenlive mpv mediainfo inkscape python-pyqt5 python-lxml telegram-desktop nvidia-open-dkms nvidia-utils opencl-nvidia qt5-xmlpatterns hddtemp psensor obsidian doublecmd-qt6 libxml2-legacy"
+            "pacman -S --needed --noconfirm sddm plasma-desktop plasm-x11-session plasma-pa plasma-systemmonitor kdeplasma-addons breeze breeze-gtk kde-gtk-config konsole thunar thunar-volman gvfs thunar-archive-plugin pipewire pipewire-pulse okular spectacle filezilla firefox firefox-ublock-origin ttf-dejavu ttf-liberation ttf-bitstream-vera cantarell-fonts ark kscreen krename kate ktorrent kolourpaint kdenlive mpv mediainfo inkscape python-pyqt5 python-lxml telegram-desktop nvidia-open-dkms nvidia-utils opencl-nvidia qt5-xmlpatterns hddtemp psensor obsidian doublecmd-qt6 libxml2-legacy"
         )
 
         with open("/etc/sddm.conf", "w") as f:
@@ -155,7 +155,7 @@ def link_soft():
     # Additional links
     os.symlink("/lib/libidn.so.12", "/lib/libidn.so.11")
     os.symlink("/lib/libtiff.so.6", "/lib/libtiff.so.5")
-    # os.symlink("/lib/libcrypt.so.2", "/lib/libcrypt.so.1")
+    os.symlink("/lib/libcrypt.so.2", "/lib/libcrypt.so.1")
     os.symlink("/lib/libpython3.14.so.1.0", "/lib/libpython3.11.so.1.0")
 
     time.sleep(1)
